7bf4814dc3acc4414d5ae13bcb8bd4330b8a0002
mahad-basit <mahadbasit.12345@gmail.com>
2026-08-12 21:09:41 +0500

    Add compute_model_output and cost_function reimplementations (Week 1)

---
+++ ng-cr1/ml-week1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def compute_model_output(x,w,b):
+    m = x.shape[0]
+    f_w_b = np.zeros(m)
+    for i in range(m):
+        f_w_b[i] = w*x[i] + b
+
+    return f_w_b
+
+def cost_function(x,y,w,b):
+    m = x.shape[0]
+
+    cost_sum = 0
+    for i in range(m):
+        cost = ((w*x[i] + b) - y[i])**2
+        cost_sum = cost_sum + cost
+
+    total_cost = (1/(2*m))*cost_sum
+
+    return total_cost 
+
+print(cost_function(np.array([2,3,4,5]), np.array([300, 400, 500 ,600]), 200, 100))
