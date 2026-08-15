120ea5e8c83e197df626da159ea77b83d2262ee7
mahad-basit <mahadbasit.12345@gmail.com>
2026-08-16 00:52:14 +0500

    Week 3: gradient_descent_logistics implemented and verified against lab reference values

--- ng-cr1/ml-week3.py
+++ ng-cr1/ml-week3.py
@@ -1,19 +1,16 @@
 import numpy as np
+import math
 
-X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])  #(m,n)
-y_train = np.array([0, 0, 0, 1, 1, 1])                                           #(m,)
-
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_train = np.array([0, 0, 0, 1, 1, 1])
 
 def sigmoid(x):
-
     x_exp = np.exp(-x)
     g = 1/(1+x_exp)
 
     return g
 
-
-def compute_cost_logistic(x, y, w, b, sigmoid):
-
+def compute_cost_logistics(x, y, w, b):
     m = x.shape[0]
     cost = 0.0
     for i in range(m):
@@ -23,10 +20,8 @@ def compute_cost_logistic(x, y, w, b, sigmoid):
     cost = cost/m
     return cost
 
-
-def compute_gradient(x, y, w, b):
+def compute_gradient_logistics(x, y, w, b):
     m,n = x.shape
-
     dj_dw = np.zeros(n)
     dj_db = 0.0
 
@@ -43,10 +38,29 @@ def compute_gradient(x, y, w, b):
 
     return dj_dw, dj_db
 
-X_tmp = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
-y_tmp = np.array([0, 0, 0, 1, 1, 1])
-w_tmp = np.array([2.,3.])
-b_tmp = 1.
-dj_dw_tmp, dj_db_tmp = compute_gradient(X_tmp, y_tmp, w_tmp, b_tmp)
-print(f"dj_db: {dj_db_tmp}" )
-print(f"dj_dw: {dj_dw_tmp.tolist()}" )
+def gradient_descent_logistics(x, y, w_in, b_in, num_iters, alpha):
+    
+    J_hist = []
+    w = w_in
+    b = b_in
+
+    for i in range(num_iters):
+        dj_dw, dj_db = compute_gradient_logistics(x,y,w,b)
+        w = w - alpha*dj_dw
+        b = b - alpha*dj_db
+
+        if i < 10000:
+            J_hist.append(compute_cost_logistics(x,y,w,b))
+
+        if i % math.ceil(num_iters/10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_hist[-1]:8.2f}")
+
+    return w, b, J_hist
+
+w_tmp  = np.zeros_like(X_train[0])
+b_tmp  = 0.
+alph = 0.1
+iters = 10000
+
+w_out, b_out, _ = gradient_descent_logistics(X_train, y_train, w_tmp, b_tmp,iters, alph) 
+print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
